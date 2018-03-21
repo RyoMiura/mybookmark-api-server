42d34de3e10f359c25e9a402ed4abfbf204f6916
ryo-miura <ryomiura0322@gmail.com>
2018-03-22 00:17:41 +0900

    create spring project and fast comit.

---
+++ src/main/java/com/mybookmark/mybookmarkapi/MybookmarkapiApplication.java
@@ -0,0 +1,12 @@
+package com.mybookmark.mybookmarkapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybookmarkapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybookmarkapiApplication.class, args);
+	}
+}
