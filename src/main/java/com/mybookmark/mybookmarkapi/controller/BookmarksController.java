d1ab693517cad8a386ec89a0ac507a062a366f3c
ryo-miura <ryomiura0322@gmail.com>
2018-03-29 00:45:53 +0900

    generate of Controller template for bookmarks and tags resource

--- src/main/java/com/mybookmark/mybookmarkapi/controller/BookmarksController.java
+++ src/main/java/com/mybookmark/mybookmarkapi/controller/BookmarksController.java
@@ -1,7 +1,10 @@
 package com.mybookmark.mybookmarkapi.controller;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,13 +16,30 @@ import com.mybookmark.mybookmarkapi.model.Bookmark;
 public class BookmarksController {
 
 	@RequestMapping(method=RequestMethod.GET, value="/{bookmarkId}")
-	public Bookmark getBookmark() {
+	public Bookmark readBookmark() {
 		return new Bookmark(1, "google", "google.com", "google site", null);
 	}
 	
-//	@RequestMapping(method=RequestMethod.GET)
-//	public Collection<Bookmark> getBookmarks() {
-//		
-//	}
+	@RequestMapping(method=RequestMethod.GET)
+	public Collection<Bookmark> readBookmarks() {
+		ArrayList<Bookmark> bookmarks = new ArrayList<>();
+		bookmarks.add(new Bookmark(1, "google", "google.com", "google site", null));
+		return bookmarks;
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public Bookmark createBookmark(@RequestBody Bookmark input) {
+		return input;
+	}
+	
+	@RequestMapping(method = RequestMethod.PUT, value = "/{bookmarkId}")
+	public Bookmark updateBookmark(@PathVariable String bookmarkId, @RequestBody Bookmark input) {
+		return input;
+	}
+	
+	@RequestMapping(method = RequestMethod.DELETE, value = "/{bookmarkId}")
+	public void deleteBookmark(@PathVariable String bookmarkId) {
+		System.out.println(bookmarkId);
+	}
 	
 }
---
+++ src/main/java/com/mybookmark/mybookmarkapi/controller/TagsController.java
@@ -0,0 +1,38 @@
+package com.mybookmark.mybookmarkapi.controller;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mybookmark.mybookmarkapi.model.Tag;
+
+@RestController
+@RequestMapping(value = "/tags")
+public class TagsController {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public Collection<Tag> readTags() {
+		ArrayList<Tag> tags = new ArrayList<>();
+		tags.add(new Tag(0, "java"));
+		tags.add(new Tag(1, "javascript"));
+		tags.add(new Tag(2, "google chrome"));
+		return tags;
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public Tag createTag(@RequestBody Tag input) {
+		System.out.println(input.getName());
+		return input;
+	}
+
+	@RequestMapping(method = RequestMethod.DELETE, value = "/{tagId}")
+	public void deleteTag(@PathVariable String tagId) {
+		System.out.println(tagId);
+	}
+
+}
