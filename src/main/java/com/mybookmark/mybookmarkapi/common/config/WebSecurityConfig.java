27d47eb744f4941eea59640b3df1f03b6138a1d1
ryo-miura <ryomiura0322@gmail.com>
2018-04-23 23:16:53 +0900

    add EnableWebSecurity annotation in WebSecurityConfig

--- src/main/java/com/mybookmark/mybookmarkapi/common/config/WebSecurityConfig.java
+++ src/main/java/com/mybookmark/mybookmarkapi/common/config/WebSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -19,6 +20,7 @@ import com.mybookmark.mybookmarkapi.common.dto.UserDto;
 import com.mybookmark.mybookmarkapi.domain.service.UserService;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
